6919caefc8c0abc02b081611e503fbc044359a6d
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-11 08:58:55 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,5 +1,5 @@
 def unsafe?(speed)
- returns true if speed is greater than 60
+ returns true if speed > 60
  returns true if speed is less than 40
  returns false if the speed is between 40 and 60
 end
