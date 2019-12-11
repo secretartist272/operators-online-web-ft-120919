8c11e31279ce1dde3d790a5effc8e9dc85d329a6
Jacqueline Freeland <secretartist272@gmail.com>
2019-12-11 09:04:27 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,14 +1,11 @@
 def unsafe?(speed)
  speed > 60 || speed < 40 ? true : false
- speed is between 40 and 60 ? 
 end
 
 
 
 def not_safe?(speed)
-  returns true if speed is greater than 60
-  returns true if speed is less than 40
-	returns false if the speed is between 40 and 60
+  
 end
 	
 
